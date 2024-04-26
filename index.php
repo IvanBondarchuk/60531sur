08944cd3f21e90f7457d19ea4368b5006d4eae04
IvanBondarchuk <bondarchuk_is@edu.surgu.ru>
2024-04-26 10:04:05 +0500

    Подключение БД

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
     require "dbconnect.php";
-
-    if ($conn->connect_error) {
-        die("Ошибка: невозможно подключиться: " . $conn->connect_error);
-    }
-
-    echo 'Подключились к базе.<br>';
-
     $result = $conn->query("SELECT * FROM publications") ;
     echo "<h2>Таблица publications</h2>";
     echo 'id'. ' '. 'id_journal'. ' '. 'name'. ' '. 'date_publication'."<br>";
