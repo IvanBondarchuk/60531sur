1a3ab5490767b324361613e5c377d547cbb27c57
IvanBondarchuk <bondarchuk_is@edu.surgu.ru>
2024-04-26 10:12:16 +0500

    Страница

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
     require "dbconnect.php";
+
     $result = $conn->query("SELECT * FROM publications") ;
     echo "<h2>Таблица publications</h2>";
     echo 'id'. ' '. 'id_journal'. ' '. 'name'. ' '. 'date_publication'."<br>";
     while ($row = $result->fetch()) {
         echo $row['id']. ' '. $row['id_journal']. ' '. $row['name']. ' '. $row['date_publication']."<br>";
 
-}
+    }
