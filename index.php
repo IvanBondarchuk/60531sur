84281fdad5c32675a2c96898498f81afdaf3d66a
IvanBondarchuk <bondarchuk_is@edu.surgu.ru>
2024-04-26 09:51:16 +0500

    Страница

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
-    require __DIR__ . '/vendor/autoload.php'; //загрузка всех установленных библиотек
-    use Dotenv\Dotenv;                        //импорт класса Dotenv из пространства имен dotenv
-    if (file_exists(__DIR__."/conf.env"))
-    {
-        $dotenv = Dotenv::createImmutable(__DIR__);
-        $dotenv->load(); //все параметры окружения помещаются в массив $_ENV
+    require "dbconnect.php";
+
+    if ($conn->connect_error) {
+        die("Ошибка: невозможно подключиться: " . $conn->connect_error);
+    }
+
+    echo 'Подключились к базе.<br>';
+
+    $result = $conn->query("SELECT * FROM publications") ;
+    echo "<h2>Таблица publications</h2>";
+    echo 'id'. ' '. 'id_journal'. ' '. 'name'. ' '. 'date_publication'."<br>";
+    while ($row = $result->fetch()) {
+        echo $row['id']. ' '. $row['id_journal']. ' '. $row['name']. ' '. $row['date_publication']."<br>";
+
 }
