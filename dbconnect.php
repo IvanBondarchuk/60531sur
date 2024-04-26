3cfa0457b64813cfb94bceb657c26f180c14eb91
IvanBondarchuk <bondarchuk_is@edu.surgu.ru>
2024-04-26 09:41:18 +0500

    БД

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
     session_start();
     require __DIR__ . '/vendor/autoload.php'; //загрузка всех установленных библиотек
     use Dotenv\Dotenv;                        //импорт класса Dotenv из пространства имен dotenv
-    if (file_exists(__DIR__."/.env"))
+    if (file_exists(__DIR__."/conf.env"))
     {
         $dotenv = Dotenv::createImmutable(__DIR__);
         $dotenv->load(); //все параметры окружения помещаются в массив $_ENV
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     require __DIR__ . '/vendor/autoload.php'; //загрузка всех установленных библиотек
     use Dotenv\Dotenv;                        //импорт класса Dotenv из пространства имен dotenv
-    if (file_exists(__DIR__."/.env"))
+    if (file_exists(__DIR__."/conf.env"))
     {
         $dotenv = Dotenv::createImmutable(__DIR__);
         $dotenv->load(); //все параметры окружения помещаются в массив $_ENV
